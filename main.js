8d2f9a3d265eb7fe08db59088b7d45f1c2028883
Noa Kim <noanonoa@gmail.com>
2020-06-10 23:45:36 -0700

    sprites added

--- main.js
+++ main.js
@@ -10,18 +10,19 @@ let gap
 let bird
 let topPipe
 let botPipe
+let ground
 let score
 /************************
 *****   FUNCTIONS:  *****
 ************************/
 //function to create new sprite
-let sprite = function(x,y, width,height, color1,color2) {
+let sprite = function(x,y, width,height, color1,color2, num1,num2,num3,num4) {
     this.x = x
     this.y = y
     this.width = width
     this.height = height
     //linear gradient color:  variable holds gradient values
-    let linear = ctx.createLinearGradient(30,0, 70,0)
+    let linear = ctx.createLinearGradient(num1,num2, num3,num4)
     //set the colors at starting point to ending point
     linear.addColorStop(0, color1)
     linear.addColorStop(1, color2)
@@ -38,9 +39,10 @@ ctx = cvs.getContext('2d')
 // gravity = 1
 // gap = 
 //bonus: let bird be an img
-bird = new sprite(30,200, 50,50, 'yellow','orange')
-topPipe = new sprite(200,0, 70,200, 'lightgreen','lightgreen')
-botPipe = new sprite(200,576, 70,-200, 'lightgreen','lightgreen')
+bird = new sprite(30,200, 50,50, 'yellow','orange', 30,0,70,0)
+topPipe = new sprite(200,0, 70,200, 'green','lightgreen', 0,100, 0,200)
+botPipe = new sprite(200,576, 70,-200, 'green','lightgreen', 0,0, 0,-80)
+ground = new sprite(0,576, 324,-70, '#d2ce89','brown', 0,0, 0,30)
 let draw = () => {
     //the bird falls by increasing y-value
     //gravity will hold increment value
@@ -48,5 +50,6 @@ let draw = () => {
     bird.render()
     topPipe.render()
     botPipe.render()
+    ground.render()
 }
 draw()
