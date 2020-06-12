2d221f4ba6c2c863a35f0ed1b0095ddd121732dd
Noa Kim <noanonoa@gmail.com>
2020-06-11 17:38:51 -0700

    sprite image updated

--- README.md
+++ README.md
@@ -14,13 +14,13 @@ A simple side-scrolling game where the player controls a bird to fly between pip
  * JavaScript  game logic
 
 # ASSETS
- * Flappy Bird (Google Images) ??? - TBD
- * Pipes (Google Images) ??? - TBD
+![flappy bird theme](/img/og-theme.png) (src: google images)
+![flappy bird theme v2](/img/og-theme-2.png) (src: https://www.spriters-resource.com/fullview/59894/)
 
 # MVP
  * start the game with a 'press button'
- * bird falls default to gravity
- * bird flies to keydown ‘spacebar’
+ * bird falls default to gravity **check**
+ * bird flies to 'click' (might be better for mobile capabilities)
  * pipes spawn top and bottom (rectangles) and scroll to the left
  * track score
  * end game on collision
@@ -48,7 +48,8 @@ A simple side-scrolling game where the player controls a bird to fly between pip
 ## JavaScript
 ### Pieces I need
  - [X] specify canvas size
- - [X] bird (bonus: [ ] image)
+ - [X] bird 
+      - [X] bonus: image)
  - [ ] top pipe (bonus: [ ] image)
  - [ ] constant gap between pipes
  - [ ] bottom pipe (bonus: [ ] image)
@@ -57,15 +58,18 @@ A simple side-scrolling game where the player controls a bird to fly between pip
  - [ ] bonus: sounds on flying, collision, and scoring
 ### Functions I need
  - [ ] start game
-    - [ ] start on keyDown
-    - [ ] spawn pipes on intervals, constant gap
+    - [ ] start on 'click' (mobile compatible)
+    - [ ] spawn set of pipes with constant gap
+      - [ ] if statement triggering at x-coordinate
  - [ ] bird's movement
     - [ ] gravity: bird's y-coordinate increases to fall
     - [ ] flying: bird's y-coordinate decreases to fly on keydown
-        - [ ] eventListener keydown seems relevant
+        - [ ] eventListener 'click'
         - [ ] bonus: flying sound
+        - [ ] animation object / array of images to flip through
     - [ ] bonus: rotate bird upward each time it flies (smells like a challenge)
  - [ ] a drawing function to animate game
+   - [ ] place images and loop
     - [ ] pipes' x-coordinates decrease to scroll left
         - [ ] increment score by 1 upon passing pipe
         - [ ] bonus: sound on passing pipe
@@ -76,8 +80,9 @@ A simple side-scrolling game where the player controls a bird to fly between pip
     - [ ] collision with pipes
     - [ ] collision with floor
     - [ ] cut screen ?
-    - [ ] restart game on keydown
+    - [ ] restart game on 'click' start button
         - [ ] display top score
+        - [ ] display current score
         - [ ] reset score on start game
 
 
@@ -91,3 +96,10 @@ A simple side-scrolling game where the player controls a bird to fly between pip
 
 # FINAL THOUGHTS
 *any progress/working comments*
+> working with generated images of rectangles `fillRect()` was proving to be more work than actually drawing canvas images from a sprite image source.  This meant working with images from the beginning would just make things simpler in the long run.
+
+> array of the pipes
+> use pipeGap to create set of pipes but in a random spot
+> var randomNumber = function() howtallthepipeis 
+> pipes = [what am i gonna put in it];   where in my code am i adding the array indeces
+> using pipes[] array to access it while in the draw() function and rendering them
--- main.js
+++ main.js
@@ -1,55 +1,113 @@
-console.log('ready to code? 🚀')
+console.log('get gravity to work 🚀')
 
 /************************
 ***** DECLARATIONS: *****
 ************************/
 let cvs
 let ctx
-let gravity
-let gap
+let pipeGap
 let bird
+let bird1
+let bird2
 let topPipe
 let botPipe
 let ground
 let score
+let theme1
+let theme2
 /************************
-*****   FUNCTIONS:  *****
+***** FUNCTIONS: ********
 ************************/
 //function to create new sprite
-let sprite = function(x,y, width,height, color1,color2, num1,num2,num3,num4) {
+let sprite = function(x,y, width,height, color) {
     this.x = x
     this.y = y
     this.width = width
     this.height = height
-    //linear gradient color:  variable holds gradient values
-    let linear = ctx.createLinearGradient(num1,num2, num3,num4)
-    //set the colors at starting point to ending point
-    linear.addColorStop(0, color1)
-    linear.addColorStop(1, color2)
+    this.gravity = 1
+    this.acceleration = .3
     this.render = function() {
         //fillStyle needs to be defined before drawing
-        ctx.fillStyle = linear
+        ctx.fillStyle = color
         //draw the rectangle
         ctx.fillRect(this.x,this.y, this.width,this.height)
     }
+    //moves the bird to gravity
+    this.newPosition = function () {
+        this.gravity += this.acceleration
+        this.y += this.gravity
+    }
 }
+
+let updateGame = () => {
+    ctx.clearRect(0,0, cvs.width,cvs.height)
+}
+//anything to be drawn on canvas goes in here
+let draw = () => {
+    ctx.fillStyle = '#00bbc4'
+    console.log(cvs.width, cvs.height)
+    ctx.fillRect(0,0, cvs.width,cvs.height)
+    bird.render()
+    bird2.render()
+}
+//animation handler
+let loop = () => {
+    draw()
+    //average of AnimationFrame is 50-60fps
+    requestAnimationFrame(loop)
+}
+
+
+
 //select <canvas> and its context
 cvs = document.getElementById('game')
 ctx = cvs.getContext('2d')
-// gravity = 1
-// gap = 
+theme1 = new Image()
+theme1.src = 'img/og-theme.png'
+theme2 = new Image()
+theme2.src = 'img/og-theme-2.png'
 //bonus: let bird be an img
-bird = new sprite(30,200, 50,50, 'yellow','orange', 30,0,70,0)
-topPipe = new sprite(200,0, 70,200, 'green','lightgreen', 0,100, 0,200)
-botPipe = new sprite(200,576, 70,-200, 'green','lightgreen', 0,0, 0,-80)
-ground = new sprite(0,576, 324,-70, '#d2ce89','brown', 0,0, 0,30)
-let draw = () => {
-    //the bird falls by increasing y-value
-    //gravity will hold increment value
-    //for loop to increase gravity
-    bird.render()
-    topPipe.render()
-    botPipe.render()
-    ground.render()
+bird = {
+    //object's key-value properties pinpointing its location
+    imgX: 276,
+    imgY: 112,
+    width: 34,
+    height: 26,
+    x: 0,
+    y: 0,
+    w: 34,
+    h: 26,
+    //object's render function that utilizes all above values to draw image onto canvas
+    render: function() {
+        ctx.drawImage(theme1, this.imgX,this.imgY,this.width,this.height, this.x,this.y,this.w,this.h)
+    }
 }
-draw()
+bird2 = {
+    //object's key-value properties pinpointing its location
+    imgX: 87,
+    imgY: 491,
+    width: 17,
+    height: 12,
+    x: 0,
+    y: 0,
+    w: 34,
+    h: 26,
+    //object's render function that utilizes all above values to draw image onto canvas
+    render: function() {
+        ctx.drawImage(theme2, this.imgX,this.imgY,this.width,this.height, this.x,this.y,this.w,this.h)
+    }
+}
+
+
+
+/******************
+***** ARCHIVE *****
+******************/
+// color1,color2, num1,num2,num3,num4) {
+            //     //linear gradient color:  variable holds gradient values
+            //     let linear = ctx.createLinearGradient(num1,num2, num3,num4)
+            //     //set the colors at starting point to ending point
+            //     linear.addColorStop(0, color1)
+            //     linear.addColorStop(1, color2)
+            //     //fillStyle needs to be defined before drawing
+            //     ctx.fillStyle = linear
